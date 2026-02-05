73e88d6fffd0eab011c9d8ccc224a3e1489dbb83
Priyanshu Saurabh <165695907+priyanshu0508@users.noreply.github.com>
2026-02-05 19:31:34 +0530

    Refactor scheduler by cleaning up comments and adding report
    Removed commented-out code and added weather report generation.

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,32 +1,3 @@
-# # import time
-# # from config import CITY_LIST
-# # from etl_pipeline import run_etl
-
-# # def start_scheduler():
-# #     while True:
-# #         for city in CITY_LIST:
-# #             run_etl(city)
-# #         time.sleep(3600)
-
-
-# import time
-# from etl_pipeline import run_etl
-# from reporter import generate_status_dashboard
-# from config import CITY_LIST
-
-# INTERVAL_SECONDS = 3600  # 1 hour
-
-# def start_scheduler():
-#     while True:
-#         for city in CITY_LIST:
-#             run_etl(city)
-
-#         # 🔥 THIS WAS MISSING
-#         generate_status_dashboard()
-
-#         time.sleep(INTERVAL_SECONDS)
-
-
 import time
 from etl_pipeline import run_etl
 from reporter import generate_weather_report, generate_status_dashboard
